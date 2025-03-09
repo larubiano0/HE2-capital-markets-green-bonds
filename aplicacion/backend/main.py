b3f3541dac2f9e88c8fdabaa850c66126a678364
larubiano0 <larubiano0@gmail.com>
2025-03-08 22:44:19 -0500

    Fixed custom domain

--- aplicacion/backend/main.py
+++ aplicacion/backend/main.py
@@ -30,10 +30,10 @@ origins = [
     "https://localhost:3001",
     # Wildcard for development on any local port (doesn't work with FastAPI's CORS middleware)
     # Add your frontend deployment URL (will update later)
-    "https://67cd0452c8aa3179c31dd588--boisterous-fairy-2dcb88.netlify.app",
+    "https://67cd0452c8aa3179c31dd588--boisterous-fairy-2dcb88.netlify.appw",
     # Add your custom domain
-    "https://yourdomain.com",
-    "https://*.yourdomain.com",
+    "https://cloudsofbogota.online",
+    "https://*.cloudsofbogota.online",
 ]
 
 app.add_middleware(
